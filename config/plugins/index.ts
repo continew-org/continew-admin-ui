7a869bf9d199bc5976df26b6104972aa42e50aff
Charles7c <charles7c@126.com>
2024-04-15 21:12:56 +0800

    chore: 移除 boxen 组件

--- config/plugins/index.ts
+++ config/plugins/index.ts
@@ -1,7 +1,6 @@
 import type { PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import appInfo from './app-info'
 
 import createAutoImport from './auto-import'
 import createComponents from './components'
@@ -9,7 +8,7 @@ import createSvgIcon from './svg-icon'
 import createMock from './mock'
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
-  const vitePlugins: (PluginOption | PluginOption[])[] = [appInfo(), vue(), vueJsx()]
+  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx()]
   vitePlugins.push(createAutoImport())
   vitePlugins.push(createComponents())
   vitePlugins.push(createSvgIcon(isBuild))
