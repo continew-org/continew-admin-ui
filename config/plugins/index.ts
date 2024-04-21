4a63a5057a73644503d2fe90085dff8c7de2cf57
Charles7c <charles7c@126.com>
2024-04-21 23:09:29 +0800

    chore: 优化开发者工具配置

--- config/plugins/index.ts
+++ config/plugins/index.ts
@@ -2,13 +2,15 @@ import type { PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import createDevtools from './devtools'
 import createAutoImport from './auto-import'
 import createComponents from './components'
 import createSvgIcon from './svg-icon'
 import createMock from './mock'
-import VueDevTools from 'vite-plugin-vue-devtools'
+
 export default function createVitePlugins(viteEnv, isBuild = false) {
-  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx(), VueDevTools()]
+  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx()]
+  vitePlugins.push(createDevtools(viteEnv))
   vitePlugins.push(createAutoImport())
   vitePlugins.push(createComponents())
   vitePlugins.push(createSvgIcon(isBuild))
