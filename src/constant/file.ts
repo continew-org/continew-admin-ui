d1af509a1aaa7d1a6982f3dcdadb7202b71b9475
Charles7c <charles7c@126.com>
2024-05-14 22:17:01 +0800

    fix: 修复文件管理图表加载错误

--- src/constant/file.ts
+++ src/constant/file.ts
@@ -1,17 +1,17 @@
 export interface FileTypeListItem {
   name: string
-  value: string
+  value: number
   icon: string
 }
 
 /** 文件分类 */
 export const FileTypeList: FileTypeListItem[] = [
-  { name: '全部', value: '0', icon: 'menu-file' },
-  { name: '图片', value: '2', icon: 'file-image-color' },
-  { name: '文档', value: '3', icon: 'file-txt' },
-  { name: '视频', value: '4', icon: 'file-video-color' },
-  { name: '音频', value: '5', icon: 'file-music' },
-  { name: '其他', value: '1', icon: 'file-other' }
+  { name: '全部', value: 0, icon: 'menu-file' },
+  { name: '图片', value: 2, icon: 'file-image-color' },
+  { name: '文档', value: 3, icon: 'file-txt' },
+  { name: '视频', value: 4, icon: 'file-video-color' },
+  { name: '音频', value: 5, icon: 'file-music' },
+  { name: '其他', value: 1, icon: 'file-other' }
 ]
 
 export interface FileExtendNameIconMap {
