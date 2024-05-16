1e83e7b955372b5d1ec877ac92dd1086e816494c
Charles7c <charles7c@126.com>
2024-05-16 21:12:45 +0800

    revert: 回退优化字典重复请求问题

--- src/hooks/app/useDict.ts
+++ src/hooks/app/useDict.ts
@@ -2,7 +2,6 @@ import { ref, toRefs } from 'vue'
 import { listCommonDict } from '@/apis'
 import { useDictStore } from '@/stores'
 
-const tmpCodeZone: string[] = []
 export function useDict(...codes: Array<string>) {
   const res = ref<any>({})
   return (() => {
@@ -13,19 +12,10 @@ export function useDict(...codes: Array<string>) {
       if (dict) {
         res.value[code] = dict
       } else {
-        if (!tmpCodeZone.includes(code)) {
-          // 防止重复请求
-          tmpCodeZone.push(code)
-          listCommonDict(code)
-            .then((resp) => {
-              res.value[code] = resp.data
-              dictStore.setDict(code, res.value[code])
-              tmpCodeZone.splice(tmpCodeZone.indexOf(code), 1)
-            })
-            .catch(() => {
-              tmpCodeZone.splice(tmpCodeZone.indexOf(code), 1)
-            })
-        }
+        listCommonDict(code).then((resp) => {
+          res.value[code] = resp.data
+          dictStore.setDict(code, res.value[code])
+        })
       }
     })
     return toRefs(res.value)
